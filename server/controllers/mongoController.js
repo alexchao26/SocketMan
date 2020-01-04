0ac528847bb6dc04b20a5d8823da8e1b51e8f3ec
alexchao26 <alexchaoo@gmail.com>
2020-01-04 06:48:04 -0500

    server-side caching all of the documents to guarantee randomizing myself...

--- server/controllers/mongoController.js
+++ server/controllers/mongoController.js
@@ -6,37 +6,29 @@ const mongoConnections = require('../models/mongoConnection');
 // connecting to the database upon server start to prevent this from happening on every request
 let qAndAModel;
 // same idea for document count, do it at server-startup & only then
-let documentCount;
+let documentCache;
 
 // connect to database, set qAndAModel and docuemntCount variables
 mongoConnections()
   .then((models) => {
     qAndAModel = models.qAndAModel;
 
-    // count the number of documents/entries in the mongoDB
-    qAndAModel.countDocuments((err, count) => {
-      documentCount = count;
+    // note: this was in an attempt to have truly randomized queries
+    // note: I'm not confident in how mongoose skips randomize
+    // cache all the docs on the server @ startup
+    qAndAModel.find({}, (err, docs) => {
+      // console.log('all docs are', docs);
+      documentCache = docs;
     });
   });
 
-
 // get one random document from the q_and_as collection
 mongoController.getNewQandA = async (req, res, next) => {
-  // note this counting and using next seems really not random...
-
-  // generate a "random" number and skip that many documents
-  const randSkip = Math.floor(Math.random() * documentCount);
-
-  // find the "random" document in the mongoDB
-  qAndAModel.findOne().skip(randSkip).exec((err2, prompt) => {
-    if (err2) {
-      // send error to global error handler with the message and a status describing a timeout
-      return next({ message: err2.message, status: 408 });
-    }
-    // console.log('prompt', prompt);
-    res.locals.newQuestion = prompt;
-    return next();
-  });
+  // generate a "random" number and grab that element of the docs cache
+  const randSkip = Math.floor(Math.random() * documentCache.length);
+  // place it on res.locals to persist prompt to next piece of middleware
+  res.locals.newQuestion = documentCache[randSkip];
+  return next();
 };
 
 module.exports = mongoController;
